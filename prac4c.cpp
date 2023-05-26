aeaa011ccc1a21ba97d7269097a5cb7ca3457208
padharyaksh <134675975+padharyaksh@users.noreply.github.com>
2023-05-26 11:14:54 +0530

    Add files via upload

---
+++ prac4c.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int circle(int r,const PI 3.14)
+{
+	int c;
+	c=PI*r*r;
+	return c;
+}
+
+int main()
+{
+	int area,r;
+	cout<<"enter the radius\n";
+	cin>>r;
+	area=circle(r);
+	cout<<"the area of circle is "<<area<<endl;
+	
+return 0;
+}
